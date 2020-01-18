dfbc09a125f18e1f80a27f80c27933c55966cb11
Cody Frazier <codyfrazier@me.com>
2020-01-18 11:37:32 -0800

    completed except for "Even Numbers Only" checkbox

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+function Stats(list){
+	const renderStats = list.list.reduce((acc,item) => {
+		if(!acc.includes(item)){
+			acc.push(item);
+		}
+		return acc;
+	},[])
+	return (<div className = 'component'>{ renderStats.length } unique numbers</div>)
+}
+
+function Form({ generateNumber }){	//Add Numbers Here
+	return (
+		<form className = 'component' onSubmit = {event => event.preventDefault()}>
+			<div>Even Numbers Only! <input id = 'check' type = 'checkbox'/></div>
+			<input type='submit' onClick = { generateNumber } value = 'Generate Number'/>
+		</form>
+	)
+}
+
+function Numbers(list){  //This is where we're listing numbers
+	return (
+		<ul className = 'component'>{
+			list.list.map((num, idx) => {
+				return (<li key = { idx }>{ num }</li>)
+			}) }
+		</ul>)
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [numList, setNumList] = useState([])
+	const flag = document.querySelector('#check');
+	const generateNumber = flag => {
+		//Still won't affect even only
+		console.log(flag);
+		const num = flag.target.checked ? Math.ceil(Math.random() * 5) * 2 : Math.ceil(Math.random() * 10);
+		console.log(flag.target.checked, num);
+		setNumList([...numList, num])
+	};
+	return (
+		<div className="App">
+			<Stats list = { numList }/>
+			<Form generateNumber = { generateNumber }/>
+			<Numbers list = { numList }/>
+		</div>
+	);
 }
 
 export default App;
